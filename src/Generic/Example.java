a9bdbebc67bc604d42eb5d53e45680e83a29d294
uknow <yunhoknight@naver.com>
2023-01-04 15:40:41 +0900

    Generic_third

--- src/Generic/Example.java
+++ src/Generic/Example.java
@@ -20,18 +20,18 @@ class User<T> {
 public class PhoneFunction {
     public static void call(User<? extends Phone> user) {
         System.out.println("-".repeat(20));
-        System.out.println("user.phone = " user.phone.getClass().getSimpleName());
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
         System.out.println("모든 Phone은 통화를 할 수 있습니다.");
     }
     public static void faceId(User<? extends IPhone> user) {
         System.out.println("-".repeat(20));
-        System.out.println("user.phone = " user.phone.getClass().getSimpleName());
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
         System.out.println("IPhone만  Face ID를 사용할 수 있습니다.");
     }
     public static void samsungPay(User<? extends Galaxy> user) {
         System.out.println("-".repeat(20));
-        System.out.println("user.phone = " user.phone.getClass().getSimpleName());
-        System.out.println("Galaxy만  삼성 페이를 사용할 수 있습니다.");
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
+        System.out.println("Galaxy만 삼성 페이를 사용할 수 있습니다.");
     }
     public static void recordVoice(User<? super Galaxy> user) {
         System.out.println("-".repeat(20));
@@ -52,32 +52,32 @@ public class Example {
 
         System.out.println("\\n######################################\\n");
 
-        PhoneFunction.faceId(new User<Phone>(new Phone()));
+//        PhoneFunction.faceId(new User<Phone>(new Phone()));
         PhoneFunction.faceId(new User<IPhone>(new IPhone()));
-        PhoneFunction.faceId(new User<Galaxy>(new Galaxy()));
+//        PhoneFunction.faceId(new User<Galaxy>(new Galaxy()));
         PhoneFunction.faceId(new User<IPhone12Pro>(new IPhone12Pro()));
         PhoneFunction.faceId(new User<IPhoneXS>(new IPhoneXS()));
-        PhoneFunction.faceId(new User<S22>(new S22()));
-        PhoneFunction.faceId(new User<ZFlip3>(new ZFlip3()));
+//        PhoneFunction.faceId(new User<S22>(new S22()));
+//        PhoneFunction.faceId(new User<ZFlip3>(new ZFlip3()));
 
         System.out.println("\\n######################################\\n");
 
-        PhoneFunction.samsungPay(new User<Phone>(new Phone()));
-        PhoneFunction.samsungPay(new User<IPhone>(new IPhone()));
+//        PhoneFunction.samsungPay(new User<Phone>(new Phone()));
+//        PhoneFunction.samsungPay(new User<IPhone>(new IPhone()));
         PhoneFunction.samsungPay(new User<Galaxy>(new Galaxy()));
-        PhoneFunction.samsungPay(new User<IPhone12Pro>(new IPhone12Pro()));
-        PhoneFunction.samsungPay(new User<IPhoneXS>(new IPhoneXS()));
+//        PhoneFunction.samsungPay(new User<IPhone12Pro>(new IPhone12Pro()));
+//        PhoneFunction.samsungPay(new User<IPhoneXS>(new IPhoneXS()));
         PhoneFunction.samsungPay(new User<S22>(new S22()));
         PhoneFunction.samsungPay(new User<ZFlip3>(new ZFlip3()));
 
         System.out.println("\\n######################################\\n");
 
         PhoneFunction.recordVoice(new User<Phone>(new Phone()));
-        PhoneFunction.recordVoice(new User<IPhone>(new IPhone()));
+//        PhoneFunction.recordVoice(new User<IPhone>(new IPhone()));
         PhoneFunction.recordVoice(new User<Galaxy>(new Galaxy()));
-        PhoneFunction.recordVoice(new User<IPhone12Pro>(new IPhone12Pro()));
-        PhoneFunction.recordVoice(new User<IPhoneXS>(new IPhoneXS()));
-        PhoneFunction.recordVoice(new User<S22>(new S22()));
-        PhoneFunction.recordVoice(new User<ZFlip3>(new ZFlip3()));
+//        PhoneFunction.recordVoice(new User<IPhone12Pro>(new IPhone12Pro()));
+//        PhoneFunction.recordVoice(new User<IPhoneXS>(new IPhoneXS()));
+//        PhoneFunction.recordVoice(new User<S22>(new S22()));
+//        PhoneFunction.recordVoice(new User<ZFlip3>(new ZFlip3()));
     }
 }
