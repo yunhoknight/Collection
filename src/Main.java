9f21a624ac149ab1d9baf78305ed6a99d3e7e3ff
uknow <yunhoknight@naver.com>
2023-01-04 11:42:13 +0900

    Enum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Level level = Level.MEDIUM;
+
+        Level[] allLevels = Level.values();
+        for (Level x : allLevels) {
+            System.out.printf("%s=%d%n", x.name(),x.ordinal());
+        }
+
+        Level findLevel = Level.valueOf("LOW");
+        System.out.println(findLevel);
+        System.out.println(Level.LOW == Level.valueOf("LOW"));
+
+        switch(level) {
+            case LOW:
+                System.out.println("낮은 레벨");
+                break;
+            case MEDIUM:
+                System.out.println("중간 레벨");
+                break;
+            case HIGH:
+                System.out.println("높은 레벨");
+                break;
+            }
     }
 }
